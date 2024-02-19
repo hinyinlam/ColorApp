cb2ffa30dfa21a7103a703ebf0403155da10695f
Hin Lam <hinl@vmware.com>
2024-02-19 15:31:53 +0800

    Add CloudFoundry Endpoint, add Info endpoint

--- build-native.sh
+++ build-native.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-./mvnw -Pnative spring-boot:build-image -DCOLOR_APP_BUILD_VERSION=0.0.5-SNAPSHOT-blue -DCOLOR_APP_DISPLAY_BGCOLOR=blue -DCOLOR_APP_DISPLAY_VERSION=1-native
+./mvnw -Pnative native:compile -DCOLOR_APP_BUILD_VERSION=0.0.5-SNAPSHOT-blue -DCOLOR_APP_DISPLAY_BGCOLOR=blue -DCOLOR_APP_DISPLAY_VERSION=1_native
+mv target/colorapp ./color-app-0.0.5-SNAPSHOT-blue
 
-./mvnw -Pnative spring-boot:build-image -DCOLOR_APP_BUILD_VERSION=0.0.5-SNAPSHOT-green -DCOLOR_APP_DISPLAY_BGCOLOR=green -DCOLOR_APP_DISPLAY_VERSION=2-native
+./mvnw -Pnative native:compile -DCOLOR_APP_BUILD_VERSION=0.0.5-SNAPSHOT-green -DCOLOR_APP_DISPLAY_BGCOLOR=green -DCOLOR_APP_DISPLAY_VERSION=2_native
+mv target/colorapp ./color-app-0.0.5-SNAPSHOT-green
+
+### Docker image with native binary (takes much longer to compile in Macbook M1)
+#./mvnw -Pnative spring-boot:build-image -DCOLOR_APP_BUILD_VERSION=0.0.5-SNAPSHOT-blue -DCOLOR_APP_DISPLAY_BGCOLOR=blue -DCOLOR_APP_DISPLAY_VERSION=1_native
+#./mvnw -Pnative spring-boot:build-image -DCOLOR_APP_BUILD_VERSION=0.0.5-SNAPSHOT-green -DCOLOR_APP_DISPLAY_BGCOLOR=green -DCOLOR_APP_DISPLAY_VERSION=2-native
